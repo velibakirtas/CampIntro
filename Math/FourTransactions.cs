af5bf9997963dfb3290d7481a6f467a1aa106913
velibakirtas <velibakirtas@outlook.com>
2022-10-09 20:22:30 +0300

    methods: void, return, default, ref, out, overloading, params

--- Math/FourTransactions.cs
+++ Math/FourTransactions.cs
@@ -8,10 +8,98 @@ namespace Math
 {
     internal class FourTransactions
     {
+        // iş (operasyon) yapan metotlar
         public void Add(int number1, int number2) 
         {
             int sum = number1 + number2;
             Console.WriteLine("Sonuç: " + sum);
         }
+        // bu metot bir operasyon metodudur. Bir işlem dizisi gerçekleştirir. İş yapan metotlar void kw ile oluşturulur
+        // metotlar içerisinde yapılan işlemlerden sonra bir veri de döndürebilir
+
+
+        // veri döndüren metotlar
+        public int Add2(int number1, int number2)
+        {
+            return number1 + number2;
+            /* var kw ile geçici değişkene de atanabilirdi 
+             *  `var result = number1 + number2;`
+             *  `return result;`
+             */  
+        }
+        // veri döndüren metotlarda döndürülecek veri return ile döndürülür
+        // metodun döndüreceği veri tipi ne ise o veri tipiyle oluşturulur. Kendi classlarımızı da kullanabiliriz
+
+
+        // default parametreli metotlar
+        public int Add3(int number1, int number2 = 30) // metotta istenen ikinci parametreyi default olarak belirledik
+        // eğer metot kullanılırken ikinci parametre değeri girilmezse ikinci parametre default 30 olarak alınacaktır
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
+
+
+        // ref keyword
+
+        /* ref keyword local scopetaki değişkendeki değişimin global scopte da geçerli olmasını sağlamak için kullanılır
+         * değer veri tipleri sadece değer temelli çalışması nedeniyle fonksiyonlar içinde belirlenen değeri kullanır ve fonksiyon görevi bittikten sonra global scopetaki değeri hala geçerli olur. Scope ile ilgili detaylı bilgi edinilebilir
+         * Yani global scopeta bulunan bir değer veri tutan değişken fonksiyon içinde farklı bir değer veriye sahip olur ve bu değer değişimi global scopetaki değeri etkilemez.
+         * Özetlenecek olursa her iki scope içinde bellekte iki farklı değişken tanımlanmış olur
+         * Biz yazdığmız fonksiyondaki değerin globaldeki değişkeni etkilemesini isteyebiliriz. ref kw bu yüzden kullanılır
+         * ref kw bellekte iki farklı değişken tanımlanmasını engelleyerek globalde tanımlanan değişkeni her iki scopeta da tek değişken olarak kullanır
+         */
+        public int Add4(int number1, int number2) 
+        {
+            number1 = 30;
+            var result = number1 + number2;
+            return result;
+        }
+
+        public int Add5(ref int number1, int number2)
+        {
+            number1 = 30;
+            var result = number1 + number2;
+            return result;
+        }
+
+
+        // out keyword 
+        public int Add6(out int number1, int number2)
+        {
+            number1 = 30;
+            var result = number1 + number2;
+            return result;
+        }
+        // out keyword de ref gibi çalışır iki farkla: ref bir değer atanmasını zorunlu kılarken out da bu zorunluluk yoktur
+        // out ile yazıldığında değişken metot içinde mutlaka set edilmelidir
+
+
+
+        // method overloading
+        public int Multiply(int number1, int number2) 
+        {
+            return number1 * number2;
+        }
+
+        public int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2* number3;
+        }
+        // metotlarda signaturelar (imzalar) farklı olduğu sürece aynı metot oluşturulabilir
+        // yukarıdkai metotların ilkinnin imzasında 2 parametre bulunurken, ikincinin imzasında 3 parametre bulunur. Dolayısıyla method overloading gerçekleşir
+
+
+
+        // params keyword
+        public int Add7(params int[] numbers)
+        {
+            var result = numbers.Sum(); // int arraylerin metotlarında olan Sum metoduyla arrayin içindeki değerlerin toplamını aldık
+            return result;
+        }
+        // params keywordü fonksiyonlara atanacak parametre sayısı belli olmadığında kullanılır
+        // implamantasyoda gönderilen parametreleri bir array içinde tutar. Dolayısıyla iterator bir yapı söz konusudur
+        // params parametreler de parametrelerin sonunda belirtilmelidir
     }
 }
--- Math/Program.cs
+++ Math/Program.cs
@@ -4,8 +4,57 @@
     {
         static void Main(string[] args)
         {
+            // iş yapan metot implamantasyonu
             FourTransactions fourTransactions = new FourTransactions();
             fourTransactions.Add(5, 6);
+
+
+
+            // veri döndüren metot implamantasyonu
+            Console.WriteLine(fourTransactions.Add2(5, 6));
+            // metodun döndüreceği değeri konsolda yazdırdık
+
+            // döndürülecek olan değeri bir değişkene atayalım
+            int result = fourTransactions.Add2(5, 6);
+            Console.WriteLine(result);
+
+
+
+            // default parametreli metot implamantasyonu
+            int sum = fourTransactions.Add3(5);
+            Console.WriteLine(sum);
+            // ikinci parametre default 30 olarak belirlendiği için 5+30 sonuncunu döndürür
+
+            int sum2 = fourTransactions.Add3(5,6);
+            Console.WriteLine(sum2);
+            // Bu sefer ikinci parametre değeri gönderildiği için default 30 değeri kullanılmaz. Sonuç: 11
+
+            // default değerler her zaman metot parametrelerinin en sonunda bulunmalıdır. Birden fazla parametre için default değer tanımlanabilir
+
+
+
+            // ref keyword kullanarak ve kullanmadan implamantasyon
+            int number1 = 10; // global scopeta number1 değişkenini tanımladık
+            Console.WriteLine(fourTransactions.Add4(number1,10)); // metot içine parametre olarak gönderdik
+            // sonuç: 40, çünkü metot içinde number1 değişkeni 30 olarak değiştirildi
+            Console.WriteLine(number1); // fakat number1 değişkenini tekrar yazdırdığımızda 10 olarak döndüğünü görebiliriz
+
+            Console.WriteLine(fourTransactions.Add5(ref number1, 10)); // metot içine parametre olarak gönderdik
+            // sonuç: 40, çünkü metot içinde number1 değişkeni 30 olarak değiştirildi
+            Console.WriteLine(number1); // bu sefer ref kullandığımız için her tek bir değişken ele alındı ve fonksiyonda değiştirilen değere eşit
+
+
+            // out keyword implamantasyon
+            int number2;
+            Console.WriteLine(fourTransactions.Add6(out number2, 10)); 
+            Console.WriteLine(number1);
+            // out keyword de ref gibi çalışır iki farkla: ref bir değer atanmasını zorunlu kılarken out da bu zorunluluk yoktur
+            // out ile yazıldığında değişken metot içinde mutlaka set edilmelidir
+
+
+            // params implamantasyon
+            int sumParams = fourTransactions.Add7(1,2,3,4,5,6,7,8,9);
+            Console.WriteLine("toplam: " + sumParams);
         }
     }
 }
